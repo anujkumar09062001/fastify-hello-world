8d7a7f88d005a952f9b13371d4ffbd265de1295b
anujkumar09062001 <72484813+anujkumar09062001@users.noreply.github.com>
2023-06-12 20:22:44 +0530

    new test 2

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ const fastify = require("fastify")({
   logger: true,
 });
 
-// try {
-//   mongoose.connect("mongodb+srv://xyz:xyz@cluster0.reo0yux.mongodb.net/Fastify?retryWrites=true&w=majority")
-//   console.log("Connected to mongodb")
-// } catch (error) {
-//   console.log("Failed to connect to mongodb")
-// }
+try {
+  mongoose.connect(
+    "mongodb+srv://xyz:xyz@cluster0.reo0yux.mongodb.net/Fastify?retryWrites=true&w=majority"
+  );
+  console.log("Connected to mongodb");
+} catch (error) {
+  console.log("Failed to connect to mongodb");
+}
 
-fastify.get("/", function (request, reply) {
-  reply.send("Hello!");
-});
+// fastify.get("/", function (request, reply) {
+//   reply.send("Hello!");
+// });
 // const app = fastify
-// const userRoute = require('./route/user.jsx');
+const userRoute = require("./route/user.jsx");
 // app.register(userRoute)
 
-// fastify.register(userRoute, {prefix: "/"})
+fastify.register(userRoute, { prefix: "/" });
 
 fastify.listen({ host: host, port: port }, function (err, address) {
   if (err) {
